b5fba9e5894e578f11619cc9bba91ec56fb6413a
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-01-27 15:16:13 +0530

    Schema updated

--- Backend/Models/TeacherModel.js
+++ Backend/Models/TeacherModel.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose')
 const teacherSchema = mongoose.Schema({
     name : {type:String,required:true},
     age : {type:Number,required:true},
-    department: { type: String, required: true},
+    department: {
+        type: String,
+        enum: ['Math', 'English', 'History', 'Geography'],
+        required: true
+    },
     createrId: { type: mongoose.Types.ObjectId, required: true },
 });
 
